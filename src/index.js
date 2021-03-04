69383aad3ef6abbd945e9de42c789e50e1c95c94
castetus <kboikov@mail.ru>
2021-03-04 12:53:43 +0300

    not working

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsArr = str.split('')
+  const resultArr = []
+  let result = true
+
+  function searchCurrentConfig(item){
+    bracketsConfig.forEach((elem, index) => {
+      if (elem.toString().indexOf(item) !== -1){
+        return index
+      }
+      return false
+    })
+  }
+
+  for (let item of bracketsArr){
+    if (searchCurrentConfig(item) === false){
+      continue
+    }
+    const config = bracketsConfig[searchCurrentConfig(item)]
+    if (config.toString().indexOf(item) === 0){
+      resultArr.push(item)
+    } else {
+      if (config[0] === resultArr[resultArr.length - 1]){
+        resultArr.unshift()
+      } else {
+        result = false
+      }
+    }
+  }
+
+  // bracketsArr.forEach((item) => {
+
+  // })
+  if (resultArr.length > 0){
+    result = false
+  }
+  return result
 }
